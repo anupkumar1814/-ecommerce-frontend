5889cd59923cfa46e8a7087ced1bf5d1ab785385
anupkumar1814 <115176086+anupkumar1814@users.noreply.github.com>
2023-06-12 16:26:10 +0530

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 
 import axios from 'axios';
 
-const URL = 'http://localhost:8000';
+const URL = 'https://ecommercewebsite-2lgx.onrender.com/';
  
 
 export const authenticateSignup = async (data) => {
